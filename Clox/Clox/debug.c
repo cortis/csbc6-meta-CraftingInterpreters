f564ef83d2d537dc052c4461097348f0537ddce3
Cortis Clark <cortis@fb.com>
2022-10-27 22:28:53 -0600

    Now handles constants
    See book section 14.5

--- Clox/Clox/debug.c
+++ Clox/Clox/debug.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "debug.h"
+#include "value.h"
 
 void disassembleChunk(Chunk* chunk, const char* name) {
     printf("== %s ==\n", name);
@@ -8,6 +9,15 @@ void disassembleChunk(Chunk* chunk, const char* name) {
     for (int offset = 0; offset < chunk->count;) {
         offset = disassembleInstruction(chunk, offset);
     }
+}
+
+static int constantInstruction(const char* name, Chunk* chunk,
+                               int offset) {
+    uint8_t constant = chunk->code[offset + 1];
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 2;
 }
 
 static int simpleInstruction(const char* name, int offset) {
@@ -20,10 +30,12 @@ int disassembleInstruction(Chunk* chunk, int offset) {
 
     uint8_t instruction = chunk->code[offset];
     switch (instruction) {
+        case OP_CONSTANT:
+            return constantInstruction("OP_CONSTANT", chunk, offset);
         case OP_RETURN:
-                return simpleInstruction("OP_RETURN", offset);
+            return simpleInstruction("OP_RETURN", offset);
         default:
-                printf("Unknown opcode %d\n", instruction);
-                return offset + 1;
+            printf("Unknown opcode %d\n", instruction);
+            return offset + 1;
     }
 }
