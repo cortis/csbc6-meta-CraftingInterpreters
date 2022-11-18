09a5f1f0a210f141182f88ad95d47b3d969fd732
cortisclark <cortis@savethemachine.com>
2022-11-18 12:47:33 -0700

    Parse Numbers
    Book: 16.3.3 (part 2)

--- Clox/Clox/scanner.c
+++ Clox/Clox/scanner.c
@@ -18,6 +18,10 @@ void initScanner(const char* source) {
     scanner.line = 1;
 }
 
+static bool isDigit(char c) {
+    return c >= '0' && c <= '9';
+}
+
 static bool isAtEnd() {
     return *scanner.current == '\0';
 }
@@ -88,6 +92,20 @@ static void skipWhitespace() {
     }
 }
 
+static Token number() {
+    while (isDigit(peek())) advance();
+    
+    // Look for a fractional part.
+    if (peek() == '.' && isDigit(peekNext())) {
+        // Consume the ".".
+        advance();
+        
+        while (isDigit(peek())) advance();
+    }
+    
+    return makeToken(TOKEN_NUMBER);
+}
+
 static Token string() {
     while (peek() != '"' && !isAtEnd()) {
         if (peek() == '\n') scanner.line++;
@@ -108,6 +126,7 @@ Token scanToken(void) {
     if (isAtEnd()) return makeToken(TOKEN_EOF);
     
     char c = advance();
+    if (isDigit(c)) return number();
     
     switch (c) {
         case '(': return makeToken(TOKEN_LEFT_PAREN);
