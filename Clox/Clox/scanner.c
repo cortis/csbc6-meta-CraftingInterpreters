2da9195c816ad85e8bf31d834be84ce152c7360c
cortisclark <cortis@savethemachine.com>
2022-11-18 12:18:55 -0700

    Added comment support
    Book 16.3.3

--- Clox/Clox/scanner.c
+++ Clox/Clox/scanner.c
@@ -31,6 +31,11 @@ static char peek() {
     return *scanner.current;
 }
 
+static char peekNext() {
+    if (isAtEnd()) return '\0';
+    return scanner.current[1];
+}
+
 static bool match(char expected) {
     if (isAtEnd()) return false;
     if (*scanner.current != expected) return false;
@@ -68,6 +73,14 @@ static void skipWhitespace() {
             case '\n':
                 scanner.line++;
                 advance();
+                break;
+            case '/':
+                if (peekNext() == '/') {
+                    // A comment goes until the end of the line.
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    return;
+                }
                 break;
             default:
                 return;
