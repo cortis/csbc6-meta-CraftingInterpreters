3dfa6454410b61dbe20ae7fa2460dd6a4e6bda5c
cortisclark <cortis@savethemachine.com>
2022-11-18 10:13:50 -0700

    Now can Add, Subtract, Multiply, Divide
    See 15.3.1

--- Clox/Clox/main.c
+++ Clox/Clox/main.c
@@ -12,16 +12,21 @@ int main(int argc, const char* argv[]) {
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
+    
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+    
+    writeChunk(&chunk, OP_ADD, 123);
+    
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+    
+    writeChunk(&chunk, OP_DIVIDE, 123);
     writeChunk(&chunk, OP_NEGATE, 123);
 
     writeChunk(&chunk, OP_RETURN, 123);
-
-    // Cortis: I disabled this line, because it just double traces
-    // the content, but as far as I can tell the book doesn't disable it
-    // Leaving it here for now so that, if needed I can easily bring it back
-    // but will probably delete soon.
-    //    disassembleChunk(&chunk, "test chunk");
-    
     interpret(&chunk);
     freeVM();
     freeChunk(&chunk);
--- Clox/Clox/vm.c
+++ Clox/Clox/vm.c
@@ -30,6 +30,12 @@ Value pop(void) {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b); \
+    } while (false)
     
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -51,6 +57,10 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
+            case OP_ADD:      BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE:   BINARY_OP(/); break;
             case OP_NEGATE:
                 push(-pop());
                 break;
