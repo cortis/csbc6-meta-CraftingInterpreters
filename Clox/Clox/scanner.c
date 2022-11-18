4c48f3d4f04a7dfa7626e9155f67a97aa445110b
cortisclark <cortis@savethemachine.com>
2022-11-18 12:44:31 -0700

    Tokenize Strings
    Book: 16.3.3 (part 1)

--- Clox/Clox/scanner.c
+++ Clox/Clox/scanner.c
@@ -88,6 +88,19 @@ static void skipWhitespace() {
     }
 }
 
+static Token string() {
+    while (peek() != '"' && !isAtEnd()) {
+        if (peek() == '\n') scanner.line++;
+        advance();
+    }
+    
+    if (isAtEnd()) return errorToken("Unterminated string.");
+    
+    // The closing quote.
+    advance();
+    return makeToken(TOKEN_STRING);
+}
+
 Token scanToken(void) {
     skipWhitespace();
     scanner.start = scanner.current;
@@ -112,6 +125,7 @@ Token scanToken(void) {
         case '=': return makeToken(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
         case '<': return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
         case '>': return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+        case '"': return string();
     }
     
     return errorToken("Unexpected character.");
